152a7b0abbad658356f5753cf585f348f4567317
Your Name <you@example.com>
2021-06-28 09:33:14 +0200

    fix WebSecurityConfig

--- LowFeeRent-Spring-5/src/main/java/com/lfr/rental/WebSecurityConfig.java
+++ LowFeeRent-Spring-5/src/main/java/com/lfr/rental/WebSecurityConfig.java
@@ -17,7 +17,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	protected void configure(HttpSecurity http) throws Exception {
 		http
 			.authorizeRequests()
-				.antMatchers("/home", "/", "/resources/**").permitAll()
+				.antMatchers("/home", "/", "/resources/**" ).permitAll()
 				.anyRequest().authenticated()
 				.and()
 			.formLogin()

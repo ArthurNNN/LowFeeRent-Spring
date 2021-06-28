1da433e1405ab80f109fb81f88430b406d055d65
Your Name <you@example.com>
2021-06-28 09:35:55 +0200

    fix

--- LowFeeRent-Spring-5/src/main/java/com/lfr/rental/WebSecurityConfig.java
+++ LowFeeRent-Spring-5/src/main/java/com/lfr/rental/WebSecurityConfig.java
@@ -18,6 +18,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 		http
 			.authorizeRequests()
 				.antMatchers("/home", "/").permitAll()
+				.antMatchers("/resources/**").permitAll() 
                 .antMatchers("/css/**").permitAll() 
                 .antMatchers("/js/**").permitAll()
                 .antMatchers("/img/**").permitAll() 
